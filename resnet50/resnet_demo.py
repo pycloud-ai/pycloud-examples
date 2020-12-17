6baa72afe6e211634640fc1df00c21ad0b8d18ea
Krzysztof Balka <krzysztof.balka@gmail.com>
2020-12-17 13:26:53 +0100

    Improve preprocessing

--- resnet50/resnet_demo.py
+++ resnet50/resnet_demo.py
@@ -18,13 +18,15 @@
 """Classify images using pretrained keras inception resnet"""
 import io
 import os
+import cv2
+import time
 import logging
 
 import numpy as np
 from PIL.Image import Image
 
-from skimage.transform import resize
-from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
+from cv2 import resize
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
 from tensorflow.keras.models import save_model, load_model
 from pycloud.core import PyCloud
 from pycloud.config import configure_logging
@@ -38,6 +40,18 @@ configure_logging()
 LOGGER = logging.getLogger("ClassificationDemo")
 
 CLOUD = PyCloud.get_instance()
+
+
+@CLOUD.endpoint("api")
+def classify(image):
+    """Put image into pipeline"""
+    result = preprocess(image)
+    return result
+
+
+@CLOUD.endpoint("api")
+def set_threshold(threshold):
+    CLOUD.broadcast(set_acc_threshold, threshold)
 
 
 @CLOUD.init_service("resnet")
@@ -64,6 +78,7 @@ def create_model():
             model, full_path, overwrite=True, include_optimizer=True, save_format=None,
             signatures=None, options=None
         )
+    
     data = {"model": model}
     return data
 
@@ -71,26 +86,43 @@ def create_model():
 @CLOUD.endpoint("resnet")
 def predict(image):
     """ Run prediction on model"""
+    t0 = time.time()
     predictions = CLOUD.initialized_data()["model"].predict(image)
+    t1 = time.time()
+    CLOUD.collect_metric("inference time", ["MAX", "MEAN"], t1-t0)
     return postprocess(predictions)
 
 
+def resize_image(image):
+    dim = (299, 299)
+    return resize(image, dim)
+   
+
 @CLOUD.endpoint("resnet")
-def pre_process(image):
+def preprocess(image):
     """Resize image to size expected by model"""
+    t0 = time.time()
     if isinstance(image, bytes):
-        image = Image.open(io.BytesIO(image))
-        image = np.array(image.convert("RGB"))
-    LOGGER.info("Input image shape: %s", len(image.shape))
-    dim = np.array((299, 299, 3))
-    image = resize(image, dim)
-    image = np.expand_dims(image, axis=0)
-    return predict(image)
+        jpg_as_np = np.frombuffer(image, dtype=np.uint8)
+        image = cv2.imdecode(jpg_as_np, flags=1)
+    t1 = time.time()
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = np.expand_dims(resize_image(image), axis=0)
+    t2 = time.time()
+    image=preprocess_input(image)
+    t3 = time.time()
+    result = predict(image)
+    t4 = time.time()
+    CLOUD.collect_metric("t1", ["MIN","MAX","MEAN"], t1-t0)
+    CLOUD.collect_metric("t2", ["MIN","MAX","MEAN"], t2-t1)
+    CLOUD.collect_metric("t3", ["MIN","MAX","MEAN"], t3-t2)
+    CLOUD.collect_metric("t3", ["MIN","MAX","MEAN"], t4-t3)
+    return result
 
 
 @CLOUD.init_service("postprocessing")
 def init_postprocessing():
-    return {"accuracy_threshold": 0.7}
+    return {"accuracy_threshold": 0.5}
 
 
 @CLOUD.queue_consumer("postprocessing")
@@ -116,24 +148,12 @@ def postprocess(predictions):
     return label
 
 
-@CLOUD.endpoint("api")
-def classify(image):
-    """Put image into pipeline"""
-    result = pre_process(image)
-    return result
-
-
-@CLOUD.endpoint("api")
-def set_threshold(threshold):
-    CLOUD.broadcast(set_acc_threshold, threshold)
-
-
 def build_app():
     """Test and build graph"""
 
     init_postprocessing()
     create_model()
-    set_threshold(0.6)
+    set_threshold(0.5)
     for path in ["images/laptop.jpeg", "images/winter.jpeg"]:
         image, path = get_image(path)
         LOGGER.info("Testing with image: %s", path)
